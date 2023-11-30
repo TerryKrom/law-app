a0084e16b5eddf9f9bb5ea8e3ab5711eefbbb1f8
TerryKrom <terrykrom1@gmail.com>
2023-11-30 19:17:44 -0300

    container modify

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './container.css'
 const Container = () => {
-    return (  
+    return (
         <>
             <div className='container'>
-
+                <div className="aside-text">
+                    <p>Conectando Consumidores ao Poder da Informação e Justiça. Juntos, Fortalecemos a Proteção do Consumidor, Construindo Confiança e Empoderamento em Cada Transação.</p>
+                </div>
+                <div className="aside-circle">
+                    <div className="circle">
+                        <p>Protegendo o consumidor.</p>
+                    </div>
+                </div>
             </div>
         </>
     );
 }
- 
+
 export default Container;

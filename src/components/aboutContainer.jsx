463821dc0143363af1345d4198be7d60f2623055
TerryKrom <terrykrom1@gmail.com>
2023-12-18 11:45:31 -0300

    Style updated

--- src/components/aboutContainer.jsx
+++ src/components/aboutContainer.jsx
@@ -6,22 +6,22 @@ const AboutContainer = () => {
         <div className="row">
             <div className="col-left">
                 <p className='about-us'>
-                    Sobre&nbsp;<span className='highlight'>nós</span> <div className="line"></div>
+                    Sobre&nbsp;<span className='highlight'>nós</span> <div className="intextLine"></div>
                 </p>
                 <div className="main-text">
                     <h2>We're committed to providing</h2>
                     <h2>the best customer service</h2>
                 </div>
                 <p className='about-us-text'>
-                    <div className="line"></div>
+                    <div className="intextLine"></div>
                     In order to meet this promise, we vow to only hire frendly, positive people that share our core values, passion for helping others and obsession with customer service.
                 </p>
 
-                <button>Explore</button>
+                <button><a href="https://consumidor.gov.br/pages/conteudo/publico/102;acoesSessaoCookie=F91B37736AA48E28495B5912122E1DE7" target="_blank" rel="noopener noreferrer">Saiba mais</a></button>
 
             </div>
             <div className="col-right">
-                <img src="/images/right-svg.svg" alt="" className='svg' />
+                {/* <img src="/images/right-svg.svg" alt="" className='svg' /> */}
             </div>
         </div>
     );
